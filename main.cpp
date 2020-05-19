0ab08db4ad87f0e6fc7cb9a31e41d8e7b0492ac1
Diulia Granatto <diuliagranatto@gmail.com>
2020-05-19 07:54:45 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@ int main()
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "-------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+
+
+    int compras;
+    double ticket;
+
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> compras;
+    cout << "Qual o ticket medio? ";
+    cin >> ticket;
     return 0;
 }
